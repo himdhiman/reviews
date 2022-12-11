0810f1abc25f34d1c538e8b3bc4d19e4be64e664
Himanshu Dhiman <45459993+himdhiman@users.noreply.github.com>
2022-12-11 14:20:12 +0530

    unCommented

--- main/spreadsheet.py
+++ main/spreadsheet.py
@@ -3,18 +3,18 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
 
-# scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
-# creds = ServiceAccountCredentials.from_json_keyfile_name(settings.CRED_PATH, scope)
-# client = gspread.authorize(creds)
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name(settings.CRED_PATH, scope)
+client = gspread.authorize(creds)
 
-# sheet_orders = client.open("Interakt Orders - For Reviews")
-# sheet_review = client.open("Interakt Reviews Data")
+sheet_orders = client.open("Interakt Orders - For Reviews")
+sheet_review = client.open("Interakt Reviews Data")
 
-# def get_data():
-#     sheet_instance = sheet_review.get_worksheet(0)
-#     data = sheet_instance.get_all_records()
-#     print(data)
-#     return data
+def get_data():
+    sheet_instance = sheet_review.get_worksheet(0)
+    data = sheet_instance.get_all_records()
+    print(data)
+    return data
 
 # # Extract and print all of the values
 # list_of_hashes = sheet.get_all_records()
--- main/views.py
+++ main/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions, status
 
-# from main.spreadsheet import get_data
+from main.spreadsheet import get_data
 
 # Create your views here.
 
@@ -14,16 +14,16 @@ class DefaultView(APIView):
     permission_classes = (permissions.AllowAny,)
 
     def post(self, request):
-        # get_data()
+        get_data()
         
         # writing Headers
-        with open(os.path.join(settings.BASE_DIR, "shared", "headers.txt"), "w+") as f:
-            data = json.dumps(str(request.headers))
-            f.write(data)
+        # with open(os.path.join(settings.BASE_DIR, "shared", "headers.txt"), "w+") as f:
+        #     data = json.dumps(str(request.headers))
+        #     f.write(data)
 
-        # writing request data
-        with open(os.path.join(settings.BASE_DIR, "shared", "data.txt"), "w+") as f:
-            data = json.dumps(request.data)
-            f.write(data)
+        # # writing request data
+        # with open(os.path.join(settings.BASE_DIR, "shared", "data.txt"), "w+") as f:
+        #     data = json.dumps(request.data)
+        #     f.write(data)
 
         return Response(status=status.HTTP_200_OK)
