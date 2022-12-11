f2183419dc49675292b8bdf166fe6678077ae39c
Himanshu Dhiman <45459993+himdhiman@users.noreply.github.com>
2022-12-11 14:32:40 +0530

    spreadsheet code modified

--- main/spreadsheet.py
+++ main/spreadsheet.py
@@ -17,5 +17,6 @@ def get_data():
     return data
 
 # # Extract and print all of the values
-list_of_hashes = sheet_orders.get_all_records()
-print(list_of_hashes)
+sheet_instance = sheet_review.get_worksheet(0)
+data = sheet_instance.get_all_records()
+print(data)
