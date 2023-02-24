422a7040c41ba13b6e7b66220d9644b5b00910b1
Himanshu Dhiman <45459993+himdhiman@users.noreply.github.com>
2023-02-24 10:11:22 +0530

    updated

--- main/views.py
+++ main/views.py
@@ -13,34 +13,37 @@ class DefaultView(APIView):
 
     def post(self, request):
         request_data = request.data
-
-        if request_data["type"] != "message_received":
+        print(request_data)
+        if request_data["type"] == "message_api_delivered" or request_data["type"] == "message_api_failed":
             req = requests.post(settings.THIRD_PARTY_URL, json=request_data, headers={
                 "Content-Type": "application/json",
             })
             print(req)
             return Response(status=status.HTTP_200_OK)
 
-        phone_number = (
-            request_data["data"]["customer"]["country_code"]
-            + request_data["data"]["customer"]["phone_number"]
-        )
+        if request_data["type"] == "message_received":
+            phone_number = (
+                request_data["data"]["customer"]["country_code"]
+                + request_data["data"]["customer"]["phone_number"]
+            )
 
-        if IsInTrackingList.is_in_tracking_list(phone_number):
-            message = request_data["data"]["message"]["message"]
-            query_object = TrackingList.objects.get(phone_number=phone_number)
-            if not query_object.first_message:
-                query_object.first_message = message
-                query_object.save()
-            elif not query_object.second_message:
-                query_object.second_message = message
-                query_object.save()
+            if IsInTrackingList.is_in_tracking_list(phone_number):
+                message = request_data["data"]["message"]["message"]
+                query_object = TrackingList.objects.get(phone_number=phone_number)
+                if not query_object.first_message:
+                    query_object.first_message = message
+                    query_object.save()
+                elif not query_object.second_message:
+                    query_object.second_message = message
+                    query_object.save()
+                return Response(status=status.HTTP_200_OK)
+
+            message, filtered_data = FilterData.filter_data(request_data)
+
+            if message == settings.TRACKING_MESSAGE:
+                TrackingList.objects.create(**filtered_data)
+                return Response(status=status.HTTP_200_OK)
+
             return Response(status=status.HTTP_200_OK)
-
-        message, filtered_data = FilterData.filter_data(request_data)
-
-        if message == settings.TRACKING_MESSAGE:
-            TrackingList.objects.create(**filtered_data)
-            return Response(status=status.HTTP_200_OK)
-
+        
         return Response(status=status.HTTP_200_OK)
